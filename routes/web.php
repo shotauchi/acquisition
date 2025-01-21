5e8607bd1ad10ebc76be4b412ab02d28c5a199af
[shotauchi] <[tianneixiang84@gmail.com]>
2025-01-21 19:28:05 +0000

    laravel-11

--- routes/web.php
+++ routes/web.php
@@ -22,6 +22,9 @@ Route::controller(NewsController::class)->prefix('admin')->name('admin.')->middl
     Route::get('news/create', 'add')->name('news.add');
     Route::post('news/create','create')->name('news.create');
     Route::get('news','index')->name('news.index');
+    Route::get('news/edit','edit')->name('news.edit');
+    Route::post('news/edit','update')->name('news.update');
+    Route::get('news/delete','delete')->name('news.delete');
 });
 
 use App\Http\Controllers\Admin\ProfileController;
@@ -31,7 +34,9 @@ Route::controller(ProfileController::class)->prefix('admin')->name('admin.')->mi
     Route::get('profile','index')->name('profile.index');
     Route::get('profile/edit','edit')->name('profile.edit');
     Route::post('profile/edit','update')->name('profile.update');
+    Route::get('profile/delete','delete')->name('profile.delete');
 });
+
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
